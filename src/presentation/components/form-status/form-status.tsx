867c27b7a5ef6122eabe5ac382f52ed129a1b925
Vinícius Barbosa de Souza <vinibrb@gmail.com>
2021-03-23 22:22:40 -0300

    refactor: add index files with multiple imports

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Styles from './form-status-styles.scss'
-import Spinner from '@/presentation/components/spinner/spinner'
+import { Spinner } from '@/presentation/components'
 
 const FormStatus: React.FC = () => {
   return (
