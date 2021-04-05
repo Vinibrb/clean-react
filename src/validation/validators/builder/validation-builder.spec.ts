bb82aad5651a542e96b720c32053826564ddd1cc
Vinícius Barbosa de Souza <vinibrb@gmail.com>
2021-04-05 14:51:16 -0300

    fix: fixed faker deprecation warnings

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -17,14 +17,14 @@ describe('ValidationBuilder', () => {
 
   test('Should return MinLengthValidation', () => {
     const field = faker.database.column()
-    const length = faker.random.number()
+    const length = faker.datatype.number()
     const validations = sut.field(field).min(length).build()
     expect(validations).toEqual([new MinLengthValidation(field, length)])
   })
 
   test('Should return a list of validations', () => {
     const field = faker.database.column()
-    const length = faker.random.number()
+    const length = faker.datatype.number()
     const validations = sut.field(field).required().min(length).email().build()
     expect(validations).toEqual([
       new RequiredFieldValidation(field),
