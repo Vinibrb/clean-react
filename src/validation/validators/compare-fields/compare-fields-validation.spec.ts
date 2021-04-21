244126ce01b818a3e344951af06cf014f4e9d8f1
Vinícius Barbosa de Souza <vinibrb@gmail.com>
2021-04-21 20:54:43 -0300

    refactor: change Validation to receive an object instead of value

---
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,31 @@
+import { CompareFieldsValidation } from './compare-fields-validation'
+import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
+
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare)
+
+describe('CompareFields', () => {
+  test('Should return error if compare is invalid', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: faker.random.word(),
+      [fieldToCompare]: faker.random.word()
+    })
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if compare is valid', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
+
+    const value = faker.random.word()
+    const error = sut.validate({
+      [field]: value,
+      [fieldToCompare]: value
+    })
+    expect(error).toBeFalsy()
+  })
+})
