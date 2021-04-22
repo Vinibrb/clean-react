eac08502b865b67e917a62c370b36fd0b6b34f54
Vinícius Barbosa de Souza <vinibrb@gmail.com>
2021-04-21 21:31:39 -0300

    fix: fix CompareFieldsValidation test being inconsistent

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -6,22 +6,22 @@ const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation
 
 describe('CompareFields', () => {
   test('Should return error if compare is invalid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
-      [field]: faker.datatype.number(),
-      [fieldToCompare]: faker.datatype.string()
+      [field]: 'any_value',
+      [fieldToCompare]: 'other_value'
     })
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if compare is valid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
+    const value = faker.random.word()
     const sut = makeSut(field, fieldToCompare)
 
-    const value = faker.random.word()
     const error = sut.validate({
       [field]: value,
       [fieldToCompare]: value
