5e4a8e208ec8127cea5551d8c5c00eb65cce9fee
Vinícius Barbosa de Souza <vinibrb@gmail.com>
2021-05-20 20:14:44 -0300

    test: ensure SignUp save accessToken on localstorage on success

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -75,4 +75,12 @@ describe('Signup', () => {
     FormHelper.testMainError('Algo de errado aconteceu. Tente novamente em breve.')
     FormHelper.testUrl('/signup')
   })
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    Http.mockOk()
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+    simulateValidSubmit()
+    FormHelper.testUrl('/')
+    FormHelper.testLocalStorageItem('accessToken')
+  })
 })
