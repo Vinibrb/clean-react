3db7f04ebd8bb2ee561b468bf2acfe59f2e0afb2
Vinícius Barbosa de Souza <vinibrb@gmail.com>
2021-05-20 20:13:06 -0300

    test: ensure SignUp present error if invalid data is returned

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -68,4 +68,11 @@ describe('Signup', () => {
     FormHelper.testMainError('Algo de errado aconteceu. Tente novamente em breve.')
     FormHelper.testUrl('/signup')
   })
+
+  it('Should present UnexpectedError if invalid data is returned', () => {
+    Http.mockInvalidData()
+    simulateValidSubmit()
+    FormHelper.testMainError('Algo de errado aconteceu. Tente novamente em breve.')
+    FormHelper.testUrl('/signup')
+  })
 })
