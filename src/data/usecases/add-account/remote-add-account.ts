9ad513dcf0ffec9fdee7cc876a202b2de7961205
Vinícius Barbosa <vinibrb@gmail.com>
2021-07-26 20:09:27 -0300

    refactor: move generic body type of http post client

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -6,8 +6,8 @@ import { EmailInUseError, UnexpectedError } from '@/domain/errors'
 export class RemoteAddAccount implements AddAccount {
   constructor (
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient<AddAccountParams, AccountModel>
-  ) {}
+    private readonly httpPostClient: HttpPostClient<AccountModel>
+  ) { }
 
   async add (params: AddAccountParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
@@ -16,9 +16,12 @@ export class RemoteAddAccount implements AddAccount {
     })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return httpResponse.body
-      case HttpStatusCode.forbidden: throw new EmailInUseError()
-      default: throw new UnexpectedError()
+      case HttpStatusCode.ok:
+        return httpResponse.body
+      case HttpStatusCode.forbidden:
+        throw new EmailInUseError()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
